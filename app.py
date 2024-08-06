30f568c5114b6eaf379eb157175a049e60c5d670
Dhie-boop <dhieumajok211@gmail.com>
2024-08-06 23:36:49 +0300

    added some routes to all the pages

--- app.py
+++ app.py
@@ -65,6 +65,35 @@ def logout():
 def dashboard():
   return render_template ('dashboard.html')
 
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/service')
+def service():
+  return render_template ('service.html')
+
+
+@app.route('/menu')
+def menu():
+  return render_template ('menu.html')
+
+@app.route('/booking')
+def booking():
+  return render_template ('booking.html')
+
+@app.route('/ourteam')
+def ourteam():
+  return render_template ('ourteam.html')
+
+@app.route('/testimonial')
+def testimonial():
+  return render_template ('testimonial.html')
+
+@app.route('/contact')
+def contact():
+  return render_template ('contact.html')
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the database tables
