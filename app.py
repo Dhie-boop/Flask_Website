0a3b7eb297696cfa96216f67c3436c9507c55bf8
Dhie-boop <dhieumajok211@gmail.com>
2024-08-06 14:38:27 +0300

    added somes new files

--- app.py
+++ app.py
@@ -92,6 +92,10 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+  
+@app.route('/dashboard')
+def dashboard():
+  return render_template ('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
