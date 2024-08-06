da868cb4b21f7f95819bad4b3b43ac44c8cd25c1
Dhie-boop <dhieumajok211@gmail.com>
2024-08-06 15:16:07 +0300

    adeed create database functions

--- app.py
+++ app.py
@@ -66,6 +66,8 @@ def dashboard():
   return render_template ('dashboard.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create the database tables
     app.run(debug=True)
 
 
